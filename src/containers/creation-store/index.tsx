ab92a6ea0e4495e833695d17997153827fc5512f
alexanderota119 <alexanderota119@gmail.com>
2022-03-20 16:35:20 +0900

    fix creation store page

--- src/containers/creation-store/index.tsx
+++ src/containers/creation-store/index.tsx
@@ -4,11 +4,24 @@ import { useMediaQuery } from 'beautiful-react-hooks';
 import { StyledContainer, Wrapper, TitleSection, Contents, SubTitleSection, Divider, MainSection, StorePhotos, TabletContents } from './styled';
 import Header from 'containers/layout/header';
 
-import Photo from 'assets/images/Photo.png';
+import StorePhoto from 'assets/images/Photo.png';
 import Bitmap6 from 'assets/images/Bitmap6.png';
 
 const CreationStore: React.FC = () => {
   const isTablet = useMediaQuery('(max-width: 639px)');
+
+  const StoprePhotoView = (
+    <StorePhotos>
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+      <img src={StorePhoto} alt="StorePhoto" />
+    </StorePhotos>
+  );
 
   return (
     <StyledContainer>
@@ -26,32 +39,14 @@ const CreationStore: React.FC = () => {
             <Divider />
             <MainSection>
               <h3>{'[Store Photos]'}</h3>
-              <StorePhotos>
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-                <img src={Photo} alt="Photo" />
-              </StorePhotos>
+              {StoprePhotoView}
               <img className="floor-plan" src={Bitmap6} alt="Bitmap6" />
             </MainSection>
           </Contents>
         ) : (
           <TabletContents>
             <span>Images/[Store Photos]</span>
-            <StorePhotos>
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-              <img src={Photo} alt="Photo" />
-            </StorePhotos>
+            {StoprePhotoView}
             <span className="tablet-floor-plan">Floor Plan</span>
             <img className="floor-plan" src={Bitmap6} alt="Bitmap6" />
           </TabletContents>
